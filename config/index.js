baa4f5503a93cbd3d43bed381256af1abea4eda6
Blashadow <blackzerogamer@gmail.com>
2016-10-05 17:39:48 +0000

    login template

--- config/index.js
+++ config/index.js
@@ -1,8 +1,18 @@
-/** Config handler **/
+"use strict";
+
+import devConfig from './dev.json';
+import prodConfig from './prod.json';
+import testConfig from './test.json';
 
 /** export **/
-module.exports = {  
-  dev: require('./dev.json'),
-  prod: require('./prod.json'),
-  test: require('./test.json')
+export default {
+    dev: devConfig,
+  
+    prod: prodConfig,
+  
+    test: testConfig,
+  
+    current: function(){
+        return devConfig;
+    }
 };
