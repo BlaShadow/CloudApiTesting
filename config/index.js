9df985d0ef033c1a1393df59036fcda56fccf6c7
Blashadow <blackzerogamer@gmail.com>
2016-10-07 19:03:04 +0000

    remove database node from each config and set it from database.json

--- config/index.js
+++ config/index.js
@@ -4,6 +4,13 @@ import devConfig from './dev.json';
 import prodConfig from './prod.json';
 import testConfig from './test.json';
 
+import databaseConfig from './database.json';
+
+/** Setup database config each enviroment config **/
+prodConfig.database = databaseConfig.production;
+devConfig.database = databaseConfig.development;
+testConfig.database = databaseConfig.test;
+
 /** export **/
 export default {
     dev: devConfig,
@@ -11,7 +18,7 @@ export default {
     prod: prodConfig,
   
     test: testConfig,
-  
+
     current: function(){
         return devConfig;
     }
